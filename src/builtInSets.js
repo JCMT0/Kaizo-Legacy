5756aab3fc89c5dbf76215e54c6b5bc6264fa13c
JCMT0 <152357710+JCMT0@users.noreply.github.com>
2026-01-13 15:26:01 -0800

    Update builtInSets.js
    First enemy (Mom) added

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -1,8 +1,9 @@
 var builtInSets = [
-	{ name: "Akhalos", setName: "Drudge", evs: { hp: 40, energy: 4, attack: 200, defense: 0, attackR: 0, defenseR: 56, speed: 200 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Ice Hammer", move2: "Wave Wrecker", move3: "Gnarly Gash", move4: "Drudge" }, posNature: "nimble", negNature: "none", veryNature: "none", ability: "Aqua Body", item: "Health Amulet", level: 50, builtIn: true },
-	{ name: "Eaglit", setName: "Mom", evs: { hp: 0, energy: 4, attack: 200, defense: 0, attackR: 184, defenseR: 0, speed: 112 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "(No Move)", move2: "(No Move)", move3: "(No Move)", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Applied Frustration", item: "none", level: 3, builtIn: true },
+	{ name: "Vambat", setName: "Mom", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Gnaw", move2: "Chomp", move3: "(No Move)", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Awakening", item: "none", level: 2, builtIn: true },
+	{ name: "Eaglit", setName: "Mom", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Pounce", move2: "Shine", move3: "(No Move)", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Applied Frustration", item: "none", level: 3, builtIn: true },
 
 ];
+
 
 
 
