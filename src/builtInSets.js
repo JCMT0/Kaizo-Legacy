e4d43c6b2056d7e3ee756a545f1b886a08d17ccb
JCMT0 <152357710+JCMT0@users.noreply.github.com>
2026-01-15 17:45:12 -0800

    Update builtInSets.js
    Added Jeremy

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -1,8 +1,11 @@
 var builtInSets = [
 	{ name: "Vambat", setName: "Mom", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Gnaw", move2: "Chomp", move3: "(No Move)", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Awakening", item: "none", level: 2, builtIn: true },
 	{ name: "Eaglit", setName: "Mom", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Pounce", move2: "Shine", move3: "(No Move)", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Applied Frustration", item: "none", level: 3, builtIn: true },
-
+	{ name: "Cathorn", setName: "Jeremy", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Bug Bite", move2: "Stare", move3: "Poison", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Defensive Priority", item: "none", level: 2, builtIn: true },
+	{ name: "Nukichi", setName: "Jeremy", evs: { hp: 0, energy: 0, attack: 0, defense: 0, attackR: 0, defenseR: 0, speed: 0 }, ivs: { hp: 40, energy: 40, attack: 40, defense: 40, attackR: 40, defenseR: 40, speed: 40 }, moves: { move1: "Stupefy", move2: "Bulk Up", move3: "Impersonate", move4: "(No Move)" }, posNature: "none", negNature: "none", veryNature: "none", ability: "Lucky", item: "none", level: 3, builtIn: true },
+	
 ];
+
 
 
 
